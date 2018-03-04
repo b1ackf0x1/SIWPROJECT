dc5751d12c594c0df8d5eb031af923dc53617430
B1ack-PC\B1ack <zampetti01@gmail.com>
2018-03-04 11:04:03 +0100

    prova 2

--- SIW/src/helloWord.java
+++ SIW/src/helloWord.java
@@ -1,5 +1,10 @@
 
 public class helloWord {
+	
+	public static void main(String args[]){
+		
+		System.out.println("\nHello word!");
+	}
 
 	public helloWord() {
 		// TODO Auto-generated constructor stub
